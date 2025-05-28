0a0e3b591c6e46b44f7f1c25f03b66d7e0f02c9b
sunhaaaaa <sunha219@naver.com>
2025-05-28 11:21:16 +0900

    week6 세션

---
+++ week6/src/data/products.js
@@ -0,0 +1,63 @@
+export const products = [
+    {
+      id: 1,
+      name: "베이직 반팔 티셔츠",
+      price: 19900,
+      image: "https://placehold.co/300x200?text=Product+1"
+    },
+    {
+      id: 2,
+      name: "슬림핏 청바지",
+      price: 29900,
+      image: "https://placehold.co/300x200?text=Product+2"
+    },
+    {
+      id: 3,
+      name: "러닝 운동화",
+      price: 49900,
+      image: "https://placehold.co/300x200?text=Product+3"
+    },
+    {
+      id: 4,
+      name: "레더 자켓",
+      price: 89900,
+      image: "https://placehold.co/300x200?text=Product+4"
+    },
+    {
+      id: 5,
+      name: "썸머 하프 팬츠",
+      price: 25900,
+      image: "https://placehold.co/300x200?text=Product+5"
+    },
+    {
+      id: 6,
+      name: "컨버스 스니커즈",
+      price: 34900,
+      image: "https://placehold.co/300x200?text=Product+6"
+    },
+    {
+      id: 7,
+      name: "울 니트 스웨터",
+      price: 39900,
+      image: "https://placehold.co/300x200?text=Product+7"
+    },
+    {
+      id: 8,
+      name: "경량 패딩",
+      price: 145900,
+      image: "https://placehold.co/300x200?text=Product+8"
+    },
+    {
+      id: 9,
+      name: "후드티",
+      price: 32900,
+      image: "https://placehold.co/300x200?text=Product+9"
+    },
+    {
+      id: 10,
+      name: "와이드 팬츠",
+      price: 37900,
+      image: "https://placehold.co/300x200?text=Product+10"
+    }
+  ];
+  
